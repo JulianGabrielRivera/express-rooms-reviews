f0c01d3c4ca7d659dcba6be4a48a46acd23da4ff
Julian Rivera <julianarevircoins@gmail.com>
2023-02-10 17:20:00 -0500

    comments and reviews

--- middleware/route-guard.js
+++ middleware/route-guard.js
@@ -1,5 +1,5 @@
 // middleware/route-guard.js
-
+const Room = require("../models/Room.model");
 // checks if the user is logged in when trying to access a specific page
 const isLoggedIn = (req, res, next) => {
   if (!req.session.user) {
@@ -17,7 +17,60 @@ const isLoggedOut = (req, res, next) => {
   next();
 };
 
+// const isOwner = (req, res, next) => {
+//   Room.findById(req.params.id)
+//     .populate("owner")
+
+//     .then((foundRoom) => {
+//       if (foundRoom.owner._id === req.session.user._id) {
+//         res.render(`/rooms/details/${req.params.id}`);
+//         next();
+//       } else if (!foundRoom.owner._id) {
+//         res.redirect("/");
+//       }
+//     })
+//     .catch((err) => console.log(err));
+// };
+const isOwner = (req, res, next) => {
+  Room.findById(req.params.id)
+    .populate("owner")
+    .then((foundRoom) => {
+      console.log(foundRoom.owner);
+      if (
+        !req.session.user ||
+        foundRoom.owner._id.toString() !== req.session.user._id
+      ) {
+        res.redirect("/rooms/all-rooms");
+      } else {
+        next();
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+const isNotOwner = (req, res, next) => {
+  Room.findById(req.params.id)
+    .populate("owner")
+    .then((foundRoom) => {
+      console.log(foundRoom.owner);
+      if (
+        !req.session.user ||
+        foundRoom.owner._id.toString() === req.session.user._id
+      ) {
+        res.render("index.hbs", { errorMessage: "You cant review." });
+      } else {
+        next();
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
 module.exports = {
   isLoggedIn,
   isLoggedOut,
+  isOwner,
+  isNotOwner,
 };
--- routes/rooms.js
+++ routes/rooms.js
@@ -1,7 +1,13 @@
 var express = require("express");
 var router = express.Router();
 const Room = require("../models/Room.model");
-const { isLoggedIn, isLoggedOut } = require("../middleware/route-guard");
+const Review = require("../models/Review.model");
+const {
+  isLoggedIn,
+  isLoggedOut,
+  isOwner,
+  isNotOwner,
+} = require("../middleware/route-guard");
 /* GET home page. */
 router.get("/all-rooms", async function (req, res, next) {
   try {
@@ -36,8 +42,76 @@ router.post("/create-room", isLoggedIn, async function (req, res, next) {
 router.get("/details/:id", async (req, res, next) => {
   const { id } = req.params;
   try {
-    const foundRoom = await Room.findById(id).populate("owner");
+    const foundRoom = await Room.findById(id)
+      .populate("owner")
+      .populate({
+        path: "reviews",
+        populate: {
+          path: "user",
+        },
+      });
+
     res.render("rooms/room-details.hbs", foundRoom);
+  } catch (error) {
+    console.log(error);
+  }
+});
+router.get("/edit/:id", isOwner, async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const foundRoom = await Room.findById(id);
+    res.render("rooms/edit-room.hbs", foundRoom);
+  } catch (error) {
+    console.log(error);
+  }
+});
+router.post("/edit/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { name, description, imageUrl } = req.body;
+    const updatedRoom = await Room.findByIdAndUpdate(
+      id,
+      {
+        name,
+        description,
+        imageUrl,
+      },
+      { new: true }
+    );
+
+    res.redirect(`/rooms/details/${id}`);
+  } catch (error) {
+    console.log(error);
+  }
+});
+router.get("/delete/:id", isOwner, async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const foundRoom = await Room.findByIdAndRemove(id);
+    console.log("Deleted Room:", foundRoom);
+    res.redirect("/rooms/all-rooms");
+  } catch (error) {
+    console.log(error);
+  }
+});
+router.post("/add-review/:id", isNotOwner, async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const createdReview = await Review.create({
+      user: req.session.user._id,
+      comment: req.body.comment,
+    });
+    const updatedRoomWithReview = await Room.findByIdAndUpdate(
+      id,
+      {
+        $push: { reviews: createdReview._id },
+      },
+      { new: true }
+    );
+
+    console.log(createdReview);
+    console.log(updatedRoomWithReview);
+    res.redirect(`/rooms/details/${id}`);
   } catch (error) {
     console.log(error);
   }
